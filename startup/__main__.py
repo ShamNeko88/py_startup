505a0664d6e920df8ce1d906b888d3294632c38e
ShamNeko <mkdvlp01@gmail.com>
2024-03-14 22:07:45 +0900

    ログ出力テスト完了

--- startup/__main__.py
+++ startup/__main__.py
@@ -1,7 +1,11 @@
+from startup.my_log_class import LogHandler
 
-
+l = LogHandler(__name__, log_file="log.txt", log_level=10, debug_mode=True, max_bytes=10000, backup_count=5, encoding="utf-8")
+l.logger.debug("処理を開始しました。")
 def main():
-  pass
+  l.logger.debug("main関数の実行")
+
+
 
 
 if __name__=="__main__":
