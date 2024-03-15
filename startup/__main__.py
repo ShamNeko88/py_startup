13ab9298b8a8d7c06c2cb63ca54c5bab45ed4b3f
ShamNeko <mkdvlp01@gmail.com>
2024-03-15 23:13:01 +0900

    整形

--- startup/__main__.py
+++ startup/__main__.py
@@ -1,7 +1,11 @@
 import sys
 from startup import config as con
 from startup.my_log_class import LogHandler
+
+
+######################################################################
 # ログ管理系のインスタンス生成
+######################################################################
 try:
     if sys.argv[1] == "d":
         debug_mode:bool = True
@@ -9,16 +13,20 @@ try:
         debug_mode:bool = False
 except IndexError:
         debug_mode:bool = False
-
 l = LogHandler(__name__, log_file="log.txt", log_level=10, debug_mode=debug_mode, max_bytes=10000, backup_count=5, encoding="utf-8")
 l.logger.debug("処理を開始しました。")
 
 
+######################################################################
+# エントリーポイント
+######################################################################
 def main():
     # 処理クラスインスタンス生成
     pc = Processing()
+
     # プロジェクト名を入力
     pr_name:str = pc.input_str("プロジェクト名を入力してください")
+
     # 設定ファイルから必要なファイルとフォルダを取得
     files:list = con.files
     l.logger.debug(f"ファイル類：{files}")
@@ -26,11 +34,14 @@ def main():
     dirs.append(pr_name)
     l.logger.debug(f"フォルダ類：{dirs}")
 
+
+######################################################################
 # 処理系クラス
+######################################################################
 class Processing():
     def __init__(self) -> None:
         l.logger.debug("インスタンス生成")
-        
+
     def input_str(self, txt):
         # コマンドラインから文字列入力
         l.logger.debug(f"コマンドラインから文字列入力：{txt}")
