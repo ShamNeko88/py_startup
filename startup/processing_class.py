c38132879d5aa482abc3d03d107303085d412dac
ShamNeko <mkdvlp01@gmail.com>
2024-03-15 22:22:12 +0900

    デバッグモード対応

--- startup/processing_class.py
+++ startup/processing_class.py
@@ -1,6 +1,16 @@
+import sys
 from startup.my_log_class import LogHandler
 
-l = LogHandler(__name__, log_file="log.txt", log_level=10, debug_mode=True, max_bytes=10000, backup_count=5, encoding="utf-8")
+# ログ管理系のインスタンス生成
+try:
+    if sys.argv[1] == "d":
+        debug_mode:bool = True
+    else:
+        debug_mode:bool = False
+except IndexError:
+        debug_mode:bool = False
+
+l = LogHandler(__name__, log_file="log.txt", log_level=10, debug_mode=debug_mode, max_bytes=10000, backup_count=5, encoding="utf-8")
 l.logger.debug(f"処理クラスでログ管理インスタンスが生成されました。")
 
 class Processing():
